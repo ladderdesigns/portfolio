ca37627f23bf002a591cff28ebb612bd66d267d4
Lucas Patel <lucaspatel@gmail.com>
2021-04-25 21:53:48 -0700

    refactor(index): refactored bio and cards section of index, started refactor on testimonials

---
+++ src/components/Card.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface Props {
+  title: string;
+  text: string | string[];
+  type: "primary" | "secondary" | "tertiary";
+}
+
+const Project = ({ title, text }: Props) => {
+  return (
+    <div className="px-4 py-3 m-2 bg-orange-500 w-96 rounded-xl">
+      <p className="text-2xl font-semibold text-white "> {title} </p>
+      <img src={"little-squiggly.svg"} className="w-24 mt-1"></img>
+      <ul className="mt-2 font-semibold text-white list-disc list-inside text-md md:text-lg">
+        <li>Lorem Ipsum is simply dummy text.</li>
+        <li> It is long established fact that a reader will be distracted. </li>
+        <li> It has survived not only five centuries, but also electronic typesetting. </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Project;
---
+++ src/components/TestimonialGroup.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
+
+interface Props {
+  name: string;
+  title: string;
+  image: string;
+  isOpen: boolean;
+  callback: Function;
+}
+
+interface TestimonialGroupConfig {
+  testimonials: Testimonial;
+}
+
+const Testimonial = ({ name, title, image, isOpen, callback }: Props) => {
+  return (
+    <>
+      <button
+        className={
+          "flex flex-row h-24 px-4 py-2 text-white items-center rounded-2xl hover:opacity-75 transition transform duration-200 " +
+          (!isOpen
+            ? "bg-white focus:w-80 w-72 transition-width rounded-lg"
+            : "bg-orange-500 w-80 transition-width rounded-lg")
+        }
+        onClick={() => callback()}
+      >
+        <div className="flex-col self-center w-40">
+          <p
+            className={
+              "text-xl font-bold text-left  " +
+              (!isOpen ? "text-orange-500" : "text-white")
+            }
+          >
+            {name}
+          </p>
+          <p
+            className={
+              "font-normal text-left text-md " +
+              (!isOpen ? "text-orange-400" : "text-white")
+            }
+          >
+            {title}
+          </p>
+        </div>
+        <div className="flex flex-col justify-center w-16">
+          <img src={image} alt="" className="w-16 h-16 rounded-full" />
+        </div>
+      </button>
+    </>
+  );
+};
+
+export default Testimonial;
